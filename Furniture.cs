83f657d95de4eee1a328290b0834a3d5c6df8a09
BinhDang <magician0811@gmail.com>
2021-03-10 16:28:41 +1100

    Add project files.

---
+++ Furniture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTest
+{
+    class Furniture
+    {
+        private string name;
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Furniture(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace SkillTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Furniture chair = new Furniture("Chair");
+            Furniture table = new Furniture("Table");
+            Furniture laptop = new Furniture("Laptop");
+            Furniture television = new Furniture("Television");
+            Furniture toothbrush = new Furniture("Toothbrush");
+            Furniture toothpick = new Furniture("Toothpick");
+
+            ArrayList furnitureList = new ArrayList();
+
+            furnitureList.Add(chair);
+            furnitureList.Add(table);
+            furnitureList.Add(laptop);
+            furnitureList.Add(television);
+            furnitureList.Add(toothbrush);
+            furnitureList.Add(toothpick);
+        }
+    }
+}
